3ce302b935cb7be251d1bd2c25c97c493d333c62
ras1k <rasikabdullah@gmail.com>
2023-04-26 18:07:23 +0600

    new shopping cart added

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,13 +1,22 @@
 const addToDb = (id) =>{
-    const quantity = localStorage.getItem(id);
-    if(quantity){
-        console.log('lol');
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
+    let shoppingCart = {};
+
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart) {
+        shoppingCart = JSON.parse(storedCart);
     } else {
-        console.log('f')
-        localStorage.setItem(id, 1);
+        shoppingCart = {};
     }
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    } else {
+        shoppingCart[id] = 1;
+        // localStorage.setItem(id, 1);
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+
 
 }
 export{addToDb}
